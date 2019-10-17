07715bd29fdde3d7a26a3c13ececd7948a0f8ba1
Akhila-rayi <56627750+Akhila-rayi@users.noreply.github.com>
2019-10-17 07:12:43 +0530

    mysixthcommit

--- app/src/main/java/com/coign/collegephonebook/AddtnlActivity.kt
+++ app/src/main/java/com/coign/collegephonebook/AddtnlActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class AddtnlActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addtnl)
         // addded to test git repository
